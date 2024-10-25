7fb6d62188dd5663d47e3707c9b167708aec02ab
FrancyelZelling <zellingf@gmail.com>
2024-10-25 20:49:00 -0300

    Move gson use to Writer class

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,6 @@ import com.zellling.models.AdressCep;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .setPrettyPrinting()
-                .create();
         Writer writer = new Writer();
         System.out.println("Digite o cep para buscar(sem espaços ou hífen):");
         var cep = scanner.nextLine();
@@ -24,7 +20,7 @@ public class Main {
 
         try {
             AdressCep adress = busca.search(cep);
-            writer.writeFile(gson.toJson(adress));
+            writer.writeFile(adress);
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizando a aplicação.");
--- src/com/zellling/Utils/Writer.java
+++ src/com/zellling/Utils/Writer.java
@@ -4,11 +4,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.zellling.models.AdressCep;
+
 public class Writer {
-  public void writeFile(String string) {
+  public void writeFile(AdressCep adressCep) {
+    Gson gson = new GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .setPrettyPrinting()
+        .create();
     try {
       FileWriter file = new FileWriter("cep.json");
-      file.write(string);
+      file.write(gson.toJson(adressCep));
       file.close();
       System.out.println("Arquivo gravado com sucesso!");
     } catch (IOException e) {
